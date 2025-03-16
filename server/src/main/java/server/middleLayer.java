5fc49b19e1919735364e679bb3006bd648d75e00
Richard Smith <smithboy.ricky@gmail.com>
2025-03-16 05:08:41 -0400

    modified:   server/src/main/java/server/middleLayer.java 	server/src/main/java/server/middleLayer.class

--- server/src/main/java/server/middleLayer.java
+++ server/src/main/java/server/middleLayer.java
@@ -22,35 +22,38 @@ public class middleLayer {
     }
     public void write(String[] val, String table) throws SQLException{
         if (table.equals("employee")){   
-            String sql = "INSERT INTO employee (employeeNum, employeeFirstName, jobTitle, employeeSirName) VALUES (?, ?, ?, ?);";
+            String sql = "INSERT INTO employee (employeeFirstName, employeeSirName, jobTitleCode, employeePassword ) VALUES (?, ?, ?, ?);";
 
             try (PreparedStatement pstmt = con.prepareStatement(sql)) {
-                pstmt.setInt(1, Integer.parseInt(val[0]));  //ID
-                pstmt.setString(2, val[1]);                 //First Name
-                pstmt.setString(3, val[2]);                 //Last Name
-                pstmt.setInt(4, Integer.parseInt(val[3]));  //Job Title
+                pstmt.setString(1, val[0]);  			//First Name
+                pstmt.setString(2, val[1]);		 	//Sir/Last Name
+                pstmt.setInt(3, Integer.parseInt(val[2]));	//Job Code
+		pstmt.setString(4, val[3]);			//Password
                 pstmt.executeUpdate();
             } catch (SQLException e) {
                 System.out.println(e.getMessage());
             }
-        }else if (table.equals("client")){   
-            String sql = "INSERT INTO client (clientFirstName, clientSirName) VALUES (?, ?);";
+        }else if (table.equals("case")){   
+            String sql = "INSERT INTO case (clientFirstName, clientSirName, jobDescription, employeeTypeNeeded, caseAccepted) VALUES (?, ?, ?, ?, ?);";
 
             try (PreparedStatement pstmt = con.prepareStatement(sql)) {
-                pstmt.setString(1, val[0]);                 //First Name
-                pstmt.setString(2, val[1]);                 //Last Name
+                pstmt.setString(1, val[0]);             	//First Name
+                pstmt.setString(2, val[1]);			//Last Name
+		pstmt.setString(3, val[2]);			//Job Desciption
+		pstmt.setInt(4, Integer.parseInt(val[3]));	//Employee Type
+		pstmt.setInt(5, Integer.parseInt(val[4]));	//Case Status
                 pstmt.executeUpdate();
             } catch (SQLException e) {
                 System.out.println(e.getMessage());
             }
         }else{
-            System.out.println("Invalid Request - Expected 'client' or 'employee'");
+            System.out.println("Invalid Request - Expected 'case' or 'employee'");
         }
     }
     public String read(String identifier, String table){
         String sql;
-        if (table.equals("client")){
-            sql = "SELECT clientFirstName, clientSirName FROM client WHERE clientNum = "+identifier+";";
+        if (table.equals("case")){
+            sql = "SELECT caseNum, clientFirstName, clientSirName, caseAccepted FROM case WHERE caseNum = "+identifier+";";
         }else if (table.equals("employee")){
             sql = "SELECT employeeFirstName, employeeSirName, jobTitle FROM employee WHERE employeeNum = "+identifier+";";
         }else{
@@ -60,8 +63,8 @@ public class middleLayer {
             pstmt.setString(1, identifier);
             try (ResultSet rs = pstmt.executeQuery()) {
                 if (rs.next()) {
-                    if (table.equals("client")) {
-                        return rs.getString(1)+'|'+rs.getString(2);
+                    if (table.equals("case")) {
+                        return rs.getString(1)+'|'+rs.getString(2)+'|'+rs.getString(3)+'|'+rs.getString(4);
                     }else {
                         return rs.getString(1)+'|'+rs.getString(2)+'|'+rs.getString(3);
                     }
